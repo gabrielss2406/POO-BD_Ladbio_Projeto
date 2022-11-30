1907418b0bcb41a23a6532849d2fd155fd7f2850
gabrielss2406 <59343269+gabrielss2406@users.noreply.github.com>
2022-11-30 11:48:25 -0300

    Delete JavaDoc3 directory

--- JavaDoc3/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"projetolabdio"},{"l":"projetolabdio.config"},{"l":"projetolabdio.controllers"},{"l":"projetolabdio.controllers.DAO"},{"l":"projetolabdio.models"},{"l":"projetolabdio.views"},{"l":"projetolabdio.views.user_auth"}];updateSearchResults();
